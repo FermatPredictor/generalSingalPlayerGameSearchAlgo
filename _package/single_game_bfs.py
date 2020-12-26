cf836a6fc40a8f84fe47c2a3d36cfbe7f9befb56
unknown <forclass021117@gmail.com>
2020-12-26 12:11:28 +0800

    can limit BFS max steps

--- _package/single_game_bfs.py
+++ _package/single_game_bfs.py
@@ -3,15 +3,16 @@ class single_game_BFS():
     def __init__(self, state):
         self.state = state
         
-    def BFS(self):
+    def BFS(self, max_step):
+        """ max_step: 最多搜索幾步 """
         seen_set = set() # 記錄哪些盤面走過了
-        queue = [(self.state.hash_value(), [])]
+        queue = [(self.state.hash_value(), [], 0)]
         while queue:
-            board, actions = queue.pop()
+            board, actions, step = queue.pop()
             if board not in seen_set:
                 seen_set.add(board)
                 self.state.setBoard(board)
                 if self.state.terminal():
-                    return actions
-                queue += [(v, actions+[k])for k, v in self.state.getValidMoves().items()]
+                    return (actions, step)
+                queue += [(v, actions+[k], step)for k, v in self.state.getValidMoves().items()]
         
